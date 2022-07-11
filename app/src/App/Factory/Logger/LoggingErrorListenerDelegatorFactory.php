1460516897e07147a89cb7720304da5a154f4c8c
Benjamin Hirsch <mail@benjaminhirsch.net>
2022-07-11 22:06:12 +0200

    Fixed factpory

--- app/src/App/Factory/Logger/LoggingErrorListenerDelegatorFactory.php
+++ app/src/App/Factory/Logger/LoggingErrorListenerDelegatorFactory.php
@@ -7,25 +7,15 @@ namespace App\Factory\Logger;
 use Interop\Container\ContainerInterface;
 use Laminas\Stratigility\Middleware\ErrorHandler;
 
-use function assert;
-use function is_array;
-use function is_callable;
-
 final class LoggingErrorListenerDelegatorFactory
 {
     /**
      * @param mixed[]|null $options
      */
-    public function __invoke(
-        ContainerInterface $container,
-        string $name,
-        callable $callback,
-        ?array $options = null
-    ): ErrorHandler {
-        $config = $container->get('config');
-        assert(is_array($config) && isset($config['logger-listener']) && is_callable($config['logger-listener']));
+    public function __invoke(ContainerInterface $container, string $name, callable $callback, ?array $options = null): ErrorHandler
+    {
         $handler = $callback();
-        $handler->attachListener($config['logger-listener']);
+        $handler->attachListener($container->get('logger-listener'));
 
         return $handler;
     }
